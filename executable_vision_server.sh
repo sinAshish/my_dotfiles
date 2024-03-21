0b10e2a7190e612cdb73bf05865a9b1d6d4b38b6
Ashish Sinha <ashishsinha108@gmail.com>
2024-03-21 13:32:02 -0700

    update dotfiles from local mac

---
+++ executable_vision_server.sh
@@ -0,0 +1,4 @@
+sshpass -p "visionlab!" ssh -o StrictHostKeyChecking=no vision@1.tcp.jp.ngrok.io -p 25318
+#sshpass -p "q1wjsfoq%)$" ssh -o StrictHostKeyChecking=no vision@:0.tcp.jp.ngrok.io -p 25318
+#screen -r mtda
+#cd ashish
